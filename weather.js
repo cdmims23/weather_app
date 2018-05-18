60ad1775b01b961ef693a9ec334068ccc40d77cc
Cori Mims <cdmims21@gmail.com>
2018-05-18 17:30:20 -0400

    Added logic to getWeather function

--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
 let info_div = $(".info-div");
+let body = $("body");
 
 function getWeather(lat, lon) {
     let celsius = 0;
@@ -9,15 +10,26 @@ function getWeather(lat, lon) {
     $.getJSON(weather_api, function(data) {
         //temperatures
         celsius += data.main.temp;
-        farenheight += celsius * 1.8 + 32;
+        farenheight += Math.round(celsius * 1.8 + 32);
 
         //Info Div
         info += "<h2>" +data.name+ "</h2>";
-        info += "<br>";
-        info += farenheight;
+        info += "<h3>" +farenheight+ "</h3>";
+        info += "<h3>" +data.weather[0].main+ "</h3>";
         info_div.html(info);
 
         console.log(data.weather[0].main)
+
+        if(data.weather[0].id >= 800) {
+            body.css("background-image", "url('images/sunny.jpg')");
+        } else if(data.weather[0].id >= 600 && data.weather[0].id <= 800) {
+            body.css("background-image", "url('images/snowy.jpg')");
+        }
+        else if(data.weather[0].id >= 200 && data.weather[0].id <= 600) {
+            body.css("background-image", "url('images/rainy.jpg')");
+        } else {
+            body.css("background-image", "url('images/sunny.jpg')");
+        }
     });
 }
 
